daaea538a1db3b2028bc12cab9dee99e1f599cc4
David Chang <dchang.me@gmail.com>
2013-05-18 13:48:49 -0700

    cleaning up app by removing passport stuff to passport-config.js

--- app.js
+++ app.js
@@ -3,60 +3,14 @@ var express = require('express')
   , path = require('path')
   , redis = require('redis')
   , client = redis.createClient()
-  , passport = require('passport')
-  , FacebookStrategy = require('passport-facebook').Strategy
-  , config = require('./config');
+  , passport = require('passport');
+
+require('./passport-config');
 
 client.on('error', function(err) {
     console.log('redis error ' + err);
 });
 
-passport.use(new FacebookStrategy({
-    clientID: config.facebook.clientID,
-    clientSecret: config.facebook.clientSecret,
-    callbackURL: '/auth/facebook/callback'
-},
-    function(accessToken, refreshToken, userData, done) {
-        client.get('users', function(err, reply) {
-
-            if(err) {
-                done(err); return;
-            }
-
-            var allUsers = {};
-            if(reply != null) {
-                try {
-                    allUsers = JSON.parse(reply);
-                    if(allUsers[userData.id]) {
-                        done(null, allUsers[userData.id]);
-                        return;
-                    }
-                } catch (err) { }
-            }
-
-            allUsers[userData.id] = userData;
-            
-            client.set('users', JSON.stringify(allUsers), function(err, reply) {
-                if(err) { done(err); return; }
-
-                done(null, userData);   
-            });
-        });
-    })
-);
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  client.get('users', function(err, reply) {
-    try {
-        var allUsers = JSON.parse(reply);
-        done(err, allUsers[id]);
-    } catch (err) { done("could not deserialize user"); }
-  });
-});
 
 var app = express();
 
