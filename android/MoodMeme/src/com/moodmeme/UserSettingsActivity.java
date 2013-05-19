f241524de5e2c173a38c445b1991e2f9b0299afd
Gary Lengkong <gary.lengkong@gmail.com>
2013-05-18 23:17:17 -0700

    Got WebView and Notification working for Android

---
+++ android/MoodMeme/src/com/moodmeme/UserSettingsActivity.java
@@ -0,0 +1,16 @@
+package com.moodmeme;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class UserSettingsActivity extends PreferenceActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+ 
+        addPreferencesFromResource(R.xml.settings);
+ 
+    }
+	
+}
